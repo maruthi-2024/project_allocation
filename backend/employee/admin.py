4dcb5b2fec1450c988d4be78683a74d741775c15
Maruthi Phanendra Chaluvadi <phanendra.chaluvadi@beehyv.com>
2024-03-10 18:19:44 +0530

    added destination

--- backend/employee/admin.py
+++ backend/employee/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import User,Designation
 
 admin.site.register(User)
+admin.site.register(Designation)
--- backend/employee/urls.py
+++ backend/employee/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 
-from .views import members 
+from .views import members,listOfUsers 
 urlpatterns = [
-    path('',members,name="employee")
+    path('',members,name="employee"),
+    path('users',listOfUsers,name="employee")
 ]
